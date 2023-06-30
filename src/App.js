96f090aaf8c99c08788266202ec4b7655d82be7d
Davis Kiprotich <mylesdavyz@gmail.com>
2023-06-30 08:50:10 +0300

    Creating calendar components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome'
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
+import { getMonth } from './util.js'
 
 function App() {
+  console.table(getMonth())
   return (
-    <div className="App">
-      <UserGreeting />
-      <NameList />
-      <ParentComponent />
-      <EventBind />
-      <ClassClick />
-      <FunctionClick />
-      <Message  />
-      <Counter />
-      <Greet name= "Miles" >
-        This is a child component
-      </Greet>
-      <Greet name= "Clark" />
-      <Greet name= "Bruce" />
-      <Welcome name="Bruce" heroName="Batman" />
-      <Welcome name="Clark" heroName="Superman" />
-      <Hello />
-    </div>
+    <React.Fragment>
+      <div></div>
+    </React.Fragment>
   );
 }
 
