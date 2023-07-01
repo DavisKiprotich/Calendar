7bc964b8c34ebd5f835493d9d3a67b18f6fc4dee
Davis Kiprotich <mylesdavyz@gmail.com>
2023-07-01 11:48:05 +0300

    Creating input components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 import { getMonth } from './util.js'
+import CalendarHeader from './components/CalendarHeader'
+import Sidebar from './components/Sidebar'
+import Month from './components/Month'
 
 function App() {
-  console.table(getMonth())
+  const [currentMonth, setCurrentMonth] = useState(getMonth())
   return (
     <React.Fragment>
-      <div></div>
+      <div className='h-screen flex flex-columns'>
+        <CalendarHeader />
+        <div className='flex flex-1 grid'>
+          <Sidebar />
+          <Month month={currentMonth} />
+        </div>
+      </div>
     </React.Fragment>
   );
 }
